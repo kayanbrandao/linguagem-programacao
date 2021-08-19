2134c0d91cf225b4e923b6b58433b33b64aa2b1d
Kayan Brandão <kayanbrandao.krb@gmail.com>
2021-08-18 20:10:55 -0400

    update

--- PrimProg.java
+++ PrimProg.java
@@ -1,6 +1,6 @@
 public class PrimProg {
 	public static void main(String[] args){
-		System.out.println("Estou determinado a aprender a programar");
-		System.out.println("Hoje é: ");
+		System.out.println("Eu, Kayan, estou determinado a aprender a programar");
+		System.out.println("Hoje é quarta-feira, 18 de agosto de 2021 ");
 	}
 }
---
+++ ReciboGasolina.java
@@ -0,0 +1,16 @@
+public class ReciboGasolina {
+    public static void main (String[] args){
+        System.out.println("+--------------------------------+");
+        System.out.println("|                                |");
+        System.out.println("|        Posto Progresso         |");
+        System.out.println("|                                |");
+        System.out.println("|       2015-03-29  16:38        |");
+        System.out.println("|                                |");
+        System.out.println("|  Quantidade:  36L              |");
+        System.out.println("|  Valor/L:      R$ 3,60         |");
+        System.out.println("|                                |");
+        System.out.println("|  Total:        R$ 129,60       |");
+        System.out.println("|                                |");
+        System.out.println("+--------------------------------+");
+    }
+}
