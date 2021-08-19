d79f40db6662af85c48537b4c9227fe4d355ab1c
Kayan Brandão <kayanbrandao.krb@gmail.com>
2021-08-18 20:22:29 -0400

    update

--- Atividades/ReciboGasolina.java
+++ Atividades/ReciboGasolina.java
@@ -2,14 +2,14 @@ public class ReciboGasolina {
     public static void main (String[] args){
         System.out.println("+--------------------------------+");
         System.out.println("|                                |");
-        System.out.println("|        Posto Progresso         |");
+        System.out.println("|        Posto Petrobras         |");
         System.out.println("|                                |");
-        System.out.println("|       2015-03-29  16:38        |");
+        System.out.println("|       2021-08-18  20:18        |");
         System.out.println("|                                |");
-        System.out.println("|  Quantidade:  36L              |");
-        System.out.println("|  Valor/L:      R$ 3,60         |");
+        System.out.println("|  Quantidade:  10L              |");
+        System.out.println("|  Valor/L:      R$ 5,56         |");
         System.out.println("|                                |");
-        System.out.println("|  Total:        R$ 129,60       |");
+        System.out.println("|  Total:        R$ 55,60        |");
         System.out.println("|                                |");
         System.out.println("+--------------------------------+");
     }
